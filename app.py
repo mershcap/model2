491af14b13717da24b6ab6e07d8e5159dccb7942
mershcap <85987187+mershcap@users.noreply.github.com>
2021-08-29 20:31:39 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask
 app=Flask(__name__)
 
+class N:
+    def __init__(self):
+        self.n=0
+    def add(self):
+        self.n=self.n+1
+        
+n=N()
+
 @app.route('/')
 def Home():
-    return "Hello world"
+    n.add
+    return n.n
 
 if __name__=='__main__':
     app.run()
